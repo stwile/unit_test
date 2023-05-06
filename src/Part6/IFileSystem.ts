bcf9813dcd3a6c63ef59554bd71d9faaba148bae
hyroky867 <handm755@gmail.com>
2023-05-06 19:54:15 +0900

    6.15 :不要なモノを削除

--- src/Part6/IFileSystem.ts
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-interface IFileSystem {
-  getFiles(directoryName: string): string[];
-  writeAllText(filePath: string, content: string): void;
-  readAllLines(filePath: string): string[];
-}
-
-export = IFileSystem;
